c14d0914c9daf6d60bb3a804edf5e05be5510bd3
Guh-paixao <gustavopaixao.dev@gmail.com>
2024-10-16 17:30:09 -0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import qrcode from 'qrcode-terminal';
+import { Client, LocalAuth, MessageMedia } from 'whatsapp-web.js';
+import { Sticker, StickerTypes } from 'wa-sticker-formatter';
+import fs from 'fs';
+import path from 'path';
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+})
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+})
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+})
+
+
+const tempDir = path.join(__dirname, 'temp');
+if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir, { recursive: true });
+}
+
+// Evento que escuta mensagens
+client.on('message', async (msg) => {
+    // Verifica se a mensagem contém mídia e se a mensagem de texto inclui "figurinha"
+    if (msg.hasMedia && msg.body.includes('Figurinha')) {
+        const media = await msg.downloadMedia();
+
+        // Gera o caminho completo para salvar a imagem
+        const imagePath = path.join(tempDir, `${msg.id.id}.jpg`);
+        fs.writeFileSync(imagePath, media.data, 'base64');
+
+        // Cria uma figurinha a partir da imagem
+        const sticker = new Sticker(imagePath, {
+            pack: 'Bot-Guh',  // Nome do pacote
+            author: 'Guh',       // Autor da figurinha
+            type: StickerTypes.FULL,  // Tipo da figurinha (FULL ou CROPPED)
+            quality: 80,         // Qualidade da figurinha
+        });
+
+        // Gera o arquivo de figurinha como um Buffer
+        const stickerBuffer = await sticker.toBuffer();
+
+        // Converte o buffer da figurinha em um objeto de mídia do WhatsApp
+        const stickerMedia = new MessageMedia('image/webp', stickerBuffer.toString('base64'));
+
+        // Envia a figurinha de volta ao usuário
+        await client.sendMessage(msg.from, stickerMedia, {
+            sendMediaAsSticker: true,
+        });
+
+        // Apaga o arquivo temporário
+        fs.unlinkSync(imagePath);
+    }
+});
+
+client.initialize();
